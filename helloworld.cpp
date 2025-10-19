9a03eebf262182a95fb875d9fa5ed6be43239c23
chiappaerica-netizen <chiappa.erica@iisleinaudi.edu.it>
2025-10-19 18:40:03 +0200

    areat.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 { 
     float baseT,altezzaT,areaT
-    cin >> (areaT=altezzaT*baseT)/2;
+    cin >> areaT=(altezzaT*baseT)/2;
     cout << "L'area del triangolo e': " << areaT;
 }
+
